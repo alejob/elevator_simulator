184f9444ea118a57b96222ec2118defd75925c2e
Alejandro Bernardin <alejo@ai>
2017-03-04 01:16:58 -0300

    Building structural work

--- elevator_sim.py
+++ elevator_sim.py
@@ -11,7 +11,15 @@ class Building:
         self.array_elevators = []
         for elevator in range(self.elevators):
             self.array_elevators.append(Elevator())
+        #init people in all the building
+        self.array_inhabitants = []
+        for current_floor in range(self.floors):
+            #self.array_inhabitants[]
+            living_floor = current_floor # init where person lives
+            for inhabitant in range(self.inhabitants_per_floor):
+                self.array_inhabitants.append(Person(800, 1700, current_floor, 0, living_floor))
         
+
     def set_elevators(self,capacity = 10, velocity = 1, current_floor = 1):
         """
         Define custom elevators
@@ -20,26 +28,30 @@ class Building:
         for elevator in range(self.elevators):            
             self.array_elevators.append(Elevator(capacity, velocity, current_floor))
     
-    def call_elevator(self, call_floor):
-        
-
+    
 
 class Elevator:
     def __init__(self, capacity = 10, velocity = 1, current_floor = 1):
         self.capacity = capacity
         self.velocity = velocity
         self.current_floor = current_floor
-"""    
-    def move(direction):
-        if direction == up:
-            up
-        elif direction == down:
-            down
+        self.calling_floor = None
+    
+    def move(self, direction):
+        if direction == "up":
+            self.current_floor += 1
+        elif direction == "down":
+            self.current_floor -= 1
         else:
-            stop
+            return 0
 
-    def sensor(call, current_floor, position_others_elevators):
+    #def sensor(calling_floor, current_floor = self.current_floor,position_others_elevators):
+    #def sensor(self, calling_floor, current_floor = 0):
+    def sensor(self, calling_floor):
+        #current_floor = self.current_floor
+        self.calling_floor = calling_floor
         #maybe this should be learned automatically
+        """
         if call > current_floor:
             go up
         elif call < current_floor:
@@ -47,12 +59,22 @@ class Elevator:
         elif call == current_floor:
             keep on floor
         #black box and ...
-        return(direction)
         
+        return(direction)
+        """
+        return calling_floor
 
 class Person:
-    def __init__(self, time_leave, time_arrive, living_floor):
+    def __init__(self, time_leave, time_arrive, current_floor, destination_floor, living_floor):
         self.time_leave = time_leave
         self.time_arrive = time_arrive
+        self.current_floor = current_floor
+        self.destination_floor = destination_floor
         self.living_floor = living_floor
-"""
+
+    def call_elevator(self, direction, array_elevators):
+        current_floor = self.current_floor #piso de llamada es el actual
+        calling_floor = current_floor
+        for elevator in array_elevators:
+            elevator.sensor(calling_floor)
+        return direction
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from elevator_sim import Building#, Elevator, Person
+from elevator_sim import Building, Elevator, Person
 
 #initialize building
 building = Building(10, 20, 2)
@@ -11,5 +11,20 @@ print building.array_elevators[0]
 print building.array_elevators[0].capacity
 print building.set_elevators(20, 2, 4)
 print building.array_elevators[0]
-print building.array_elevators[0].current_floor
+print "piso ascensor", building.array_elevators[0].current_floor
 print building.array_elevators
+print len(building.array_inhabitants)
+print building.array_inhabitants[134].current_floor
+
+print "piso ascensor", building.array_elevators[0].current_floor
+print building.array_elevators[0].move("down")
+print "piso ascensor", building.array_elevators[0].current_floor
+
+print "ESTADO FINAL"
+print "ascensor A, piso: ", building.array_elevators[0].current_floor
+print "ascensor B, piso: ", building.array_elevators[1].current_floor
+
+print building.array_inhabitants[134].call_elevator("down",building.array_elevators)
+
+print "ascensor A, calling: ", building.array_elevators[0].calling_floor
+print "ascensor B, calling: ", building.array_elevators[1].calling_floor
