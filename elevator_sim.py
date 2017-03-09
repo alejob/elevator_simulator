33f539b0e7e43289510caf79727b3dfc65e4e49e
Alejandro Bernardin <bernardin.alejandro@gmail.com>
2017-03-09 18:27:09 -0300

    added brain to the building

--- elevator_sim.py
+++ elevator_sim.py
@@ -3,10 +3,13 @@ import numpy as np
 
 class Building:
     def __init__(self, floors, inhabitants_per_floor, elevators):
+        #floors doesn't include level 0, ex: floor = 10, mean 10 floor + level 0
         self.floors = floors
         self.inhabitants_per_floor = inhabitants_per_floor
         self.elevators = elevators
         self.total_inhabitants = inhabitants_per_floor * floors
+        #Create a building with a brain
+        self.brain = Brain()
         #init defaults elevators
         self.array_elevators = []
         for elevator in range(self.elevators):
@@ -17,7 +20,8 @@ class Building:
             #self.array_inhabitants[]
             living_floor = current_floor # init where person lives
             for inhabitant in range(self.inhabitants_per_floor):
-                self.array_inhabitants.append(Person(800, 1700, current_floor, 0, living_floor))
+                #with the last "self" I call Building Class and set to the inhabitant
+                self.array_inhabitants.append(Person(800, 1700, current_floor+1, 0, living_floor, self))
         
 
     def set_elevators(self,capacity = 10, velocity = 1, current_floor = 1):
@@ -29,7 +33,6 @@ class Building:
             self.array_elevators.append(Elevator(capacity, velocity, current_floor))
     
     
-
 class Elevator:
     def __init__(self, capacity = 10, velocity = 1, current_floor = 1):
         self.capacity = capacity
@@ -64,17 +67,36 @@ class Elevator:
         """
         return calling_floor
 
+
 class Person:
-    def __init__(self, time_leave, time_arrive, current_floor, destination_floor, living_floor):
+    def __init__(self, time_leave, time_arrive, current_floor, destination_floor, living_floor, current_building):
         self.time_leave = time_leave
         self.time_arrive = time_arrive
         self.current_floor = current_floor
         self.destination_floor = destination_floor
         self.living_floor = living_floor
-
-    def call_elevator(self, direction, array_elevators):
-        current_floor = self.current_floor #piso de llamada es el actual
+        self.building = current_building
+        
+    def call_elevator(self, direction):
+        current_floor = self.current_floor #calling floor is the current floor
         calling_floor = current_floor
-        for elevator in array_elevators:
-            elevator.sensor(calling_floor)
-        return direction
+        self.building.brain.calling_elevator(calling_floor, direction)
+        
+    #destiny floor
+    def set_elevator(self, destination_floor):
+        self.building.brain.destination_floor(destination_floor)
+
+
+class Brain:
+    #def __init__(self,buildingg):
+    #    self.buildingg = buildingg
+        
+    def calling_elevator(self, calling_floor, direction):
+        print "Brain: orden recibida, llamado desde el piso", calling_floor
+        return 0
+    
+    def destination_floor(self, destination_floor):
+        print "Brain: orden recibida, ir al piso", destination_floor
+        return 0
+    
+    
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from elevator_sim import Building, Elevator, Person
+from elevator_sim import Building, Elevator, Person, Brain
+
+import numpy as np
 
 #initialize building
 building = Building(10, 20, 2)
@@ -6,6 +8,7 @@ building = Building(10, 20, 2)
 
 #print building.create_elevators()
 
+"""
 print building.floors
 print building.array_elevators[0]
 print building.array_elevators[0].capacity
@@ -24,7 +27,31 @@ print "ESTADO FINAL"
 print "ascensor A, piso: ", building.array_elevators[0].current_floor
 print "ascensor B, piso: ", building.array_elevators[1].current_floor
 
-print building.array_inhabitants[134].call_elevator("down",building.array_elevators)
+#print building.array_inhabitants[134].call_elevator("down",building.array_elevators)
+
 
 print "ascensor A, calling: ", building.array_elevators[0].calling_floor
 print "ascensor B, calling: ", building.array_elevators[1].calling_floor
+
+#building_brain = Brain(building)
+"""
+#print building.array_inhabitants[134].call_elevator("down", building_brain)
+print building.array_inhabitants[134].call_elevator("down")
+#print building.brain
+print building.array_inhabitants[134].set_elevator(0)
+
+
+for i in range(1000):
+    select_inhab = np.random.randint(len(building.array_inhabitants))
+    print select_inhab
+    if building.array_inhabitants[select_inhab].current_floor == 0:
+        building.array_inhabitants[select_inhab].call_elevator("up")
+        living_floor = building.array_inhabitants[select_inhab].living_floor
+        building.array_inhabitants[select_inhab].set_elevator(living_floor)
+        building.array_inhabitants[select_inhab].current_floor = living_floor
+    else:
+        building.array_inhabitants[select_inhab].call_elevator("down")
+        building.array_inhabitants[select_inhab].set_elevator(0)
+        building.array_inhabitants[select_inhab].current_floor = 0
+        
+    print "current floor", building.array_inhabitants[select_inhab].current_floor
